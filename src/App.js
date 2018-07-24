decc2030fd053d6e3f75bee6039db83230c2435f
carloscastanedan <carlos.castaneda@beerhouse.mx>
2018-07-24 14:57:04 -0500

    app component was modified

--- src/App.js
+++ src/App.js
@@ -6,8 +6,34 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to static Web - I usign continuos -integration</h1>
+          <h1 className="App-title">Title</h1>
         </header>
+        <div className="main">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Duis eu aliquam ligula. In purus nibh, volutpat in est vitae, sagittis molestie massa. 
+            Pellentesque varius nulla non ante luctus, quis egestas nisi aliquet. 
+            Etiam posuere tortor sed aliquam commodo. Nam ut mi magna. Morbi at laoreet diam, sit amet ultrices metus.
+            Morbi mi turpis, feugiat at porttitor sit amet, efficitur et enim. Nam feugiat, 
+            eros nec mattis aliquet, nunc velit facilisis lacus, porta pretium erat nibh sit amet augue.
+            Nam vitae fringilla nisl, at gravida nunc. Fusce vel mi finibus ligula vulputate scelerisque vel eu ante.
+          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Duis eu aliquam ligula. In purus nibh, volutpat in est vitae, sagittis molestie massa. 
+            Pellentesque varius nulla non ante luctus, quis egestas nisi aliquet. 
+            Etiam posuere tortor sed aliquam commodo. Nam ut mi magna. Morbi at laoreet diam, sit amet ultrices metus.
+            Morbi mi turpis, feugiat at porttitor sit amet, efficitur et enim. Nam feugiat, 
+            eros nec mattis aliquet, nunc velit facilisis lacus, porta pretium erat nibh sit amet augue.
+            Nam vitae fringilla nisl, at gravida nunc. Fusce vel mi finibus ligula vulputate scelerisque vel eu ante.
+          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Duis eu aliquam ligula. In purus nibh, volutpat in est vitae, sagittis molestie massa. 
+            Pellentesque varius nulla non ante luctus, quis egestas nisi aliquet. 
+            Etiam posuere tortor sed aliquam commodo. Nam ut mi magna. Morbi at laoreet diam, sit amet ultrices metus.
+            Morbi mi turpis, feugiat at porttitor sit amet, efficitur et enim. Nam feugiat, 
+            eros nec mattis aliquet, nunc velit facilisis lacus, porta pretium erat nibh sit amet augue.
+            Nam vitae fringilla nisl, at gravida nunc. Fusce vel mi finibus ligula vulputate scelerisque vel eu ante.
+          </p>
+        </div>
       </div>
     );
   }
